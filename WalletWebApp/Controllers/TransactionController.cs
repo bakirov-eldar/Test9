74fe4e67e368bdcae137c8428b8728a90031cbd6
Eldar Bakirov <e.bakirov@bakirovlab.ru>
2024-06-22 15:05:35 +0600

    Add Transfer actions with viewmodel, view and locazalitons

--- WalletWebApp/Controllers/TransactionController.cs
+++ WalletWebApp/Controllers/TransactionController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
@@ -49,6 +50,59 @@ public class TransactionController : Controller
                 _walletContext.Transactions.Add(transaction);
                 await _userManager.UpdateAsync(user);
                 return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("WalletNumber", _localizer["UnknownWallet"]);
+            }
+        }
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Transfer()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> Transfer(TransferViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var receiver = await _userManager.Users.FirstOrDefaultAsync(e => e.WalletNumber == model.WalletNumber);
+            if (receiver is not null)
+            {
+                var sender = await _userManager.GetUserAsync(User);
+                if (sender.Id != receiver.Id)
+                {
+                    var newBalance = sender.Balance - model.Amount;
+                    if (newBalance >= 0)
+                    {
+                        sender.Balance = newBalance;
+                        receiver.Balance += model.Amount;
+                        var transaction = new Transaction()
+                        {
+                            Amount = model.Amount,
+                            FromUserId = sender.Id,
+                            ToUserId = receiver.Id,
+                            Type = TransactionType.Transfer
+                        };
+                        _walletContext.Transactions.Add(transaction);
+                        await _userManager.UpdateAsync(sender);
+                        await _userManager.UpdateAsync(receiver);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Amount", _localizer["InsufficientFunds"]);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("WalletNumber", _localizer["MatchIds"]);
+                }
             }
             else
             {
