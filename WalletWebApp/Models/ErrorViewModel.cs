4533207fb2545b326dd366fa01c2ebd5337fc82a
Eldar Bakirov <e.bakirov@bakirovlab.ru>
2024-06-22 11:17:27 +0600

    Add project files

---
+++ WalletWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WalletWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
