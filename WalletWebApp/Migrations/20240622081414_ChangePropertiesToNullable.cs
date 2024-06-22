558d096eac09937ab88f29ccebc7a0530f706efd
Eldar Bakirov <e.bakirov@bakirovlab.ru>
2024-06-22 14:14:41 +0600

    Add ChangePropertiesToNullable migration

---
+++ WalletWebApp/Migrations/20240622081414_ChangePropertiesToNullable.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WalletWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePropertiesToNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_AspNetUsers_FromUserId",
+                table: "Transactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_AspNetUsers_ToUserId",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ToUserId",
+                table: "Transactions",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FromUserId",
+                table: "Transactions",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_AspNetUsers_FromUserId",
+                table: "Transactions",
+                column: "FromUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_AspNetUsers_ToUserId",
+                table: "Transactions",
+                column: "ToUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_AspNetUsers_FromUserId",
+                table: "Transactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_AspNetUsers_ToUserId",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ToUserId",
+                table: "Transactions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FromUserId",
+                table: "Transactions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_AspNetUsers_FromUserId",
+                table: "Transactions",
+                column: "FromUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_AspNetUsers_ToUserId",
+                table: "Transactions",
+                column: "ToUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- WalletWebApp/Models/Transaction.cs
+++ WalletWebApp/Models/Transaction.cs
@@ -3,9 +3,9 @@
 public class Transaction
 {
     public int Id { get; set; }
-    public int FromUserId { get; set; }
-    public User FromUser { get; set; }
-    public int ToUserId { get; set; }
-    public User ToUser { get; set; }
+    public int? FromUserId { get; set; }
+    public User? FromUser { get; set; }
+    public int? ToUserId { get; set; }
+    public User? ToUser { get; set; }
     public decimal Amount { get; set; }
 }
